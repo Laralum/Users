8fcc3a3edd70d39a4de82487cd0c1af625976c0e
Erik Campobadal <soc@erik.cat>
2017-02-10 00:55:43 +0100

    Tweaks

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -9,5 +9,5 @@ Route::group([
         // First the suplementor, then the resource
         // https://laravel.com/docs/5.4/controllers#resource-controllers
         Route::get('users/{user}/delete', 'UserController@confirmDelete')->name('users.destroy.confirm');
-        Route::resource('users', 'UserController');
+        Route::resource('users', 'UserController', ['except' => ['show']]);
 });
