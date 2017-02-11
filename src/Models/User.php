d093333a98a1d62f1a4cdda34929736a358a14d8
24aitor <1aitorribaarchilla@gmail.com>
2017-02-11 20:29:04 +0100

    User model tweaks

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,8 +3,29 @@
 namespace Laralum\Users\Models;
 
 use App\User as ExtendUser;
+use File;
 
 class User extends ExtendUser
 {
-    //
+    /**
+    * Returns the user avatar.
+    */
+    public function avatar()
+    {
+        if(File::exists(public_path('/avatars'.'/'.md5($this->email)))){
+            return asset('/avatars'.'/'.md5($this->email));
+        }
+        return "https://tracker.moodle.org/secure/attachment/30912/f3.png";
+    }
+
+    /**
+    * Returns the a boolean for know if user has avatar.
+    */
+    public function hasAvatar()
+    {
+        if(File::exists(public_path('/avatars/'.md5($this->email)))){
+            return true;
+        }
+        return false;
+    }
 }
