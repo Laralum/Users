4b8cce3d64290d9c4bb56895d244ce803101a8f9
Erik Campobadal <soc@erik.cat>
2017-02-20 16:04:30 +0100

    Tweaks

--- src/Views/create.blade.php
+++ src/Views/create.blade.php
@@ -4,7 +4,7 @@
 @section('subtitle', trans('laralum_users::general.create_user_desc'))
 @section('breadcrumb')
     <ul class="uk-breadcrumb">
-        <li><a href="{{ route('laralum::index') }}">@lang('laralum_tickets::general.home')</a></li>
+        <li><a href="{{ route('laralum::index') }}">@lang('laralum_users::general.home')</a></li>
         <li><a href="{{ route('laralum::users.index') }}">@lang('laralum_users::general.user_list')</a></li>
         <li><span>@lang('laralum_users::general.create_user')</span></li>
     </ul>
