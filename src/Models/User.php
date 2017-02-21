a9f7c8bed0f58448f5431434be6f71fd0978c5b9
Erik Campobadal <soc@erik.cat>
2017-02-21 17:09:37 +0100

    Relationships

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,10 +3,80 @@
 namespace Laralum\Users\Models;
 
 use App\User as ExtendUser;
+use Laralum\Roles\Models\Role;
+use Laralum\Permisions\Models\Permision;
 use File;
 
 class User extends ExtendUser
 {
+    /**
+    * Returns the user roles.
+    */
+    public function roles()
+    {
+        return $this->belongsToMany('Laralum\Roles\Models\Role');
+    }
+
+    /**
+    * Returns if the user has a permission.
+    * @param \Laralum\Permissions\Models\Permision $permision
+    * @return bool
+    */
+    public function hasPermissions(Permision $permision)
+    {
+        foreach( $this->roles as $role ) {
+            foreach( $role->permissions as $p ) {
+                ($p->id != $permision->id) ?: return true;
+            }
+        }
+
+        return false
+    }
+
+    /**
+    * Returns if the user has a permission.
+    * @param string $permision
+    * @return bool
+    */
+    public function hasPermissionsBySlug($slug)
+    {
+        foreach( $this->roles as $role ) {
+            foreach( $role->permissions as $p ) {
+                ($p->slug != $slug) ?: return true;
+            }
+        }
+
+        return false
+    }
+
+    /**
+    * Returns if the user has a role.
+    * @param \Laralum\Roles\Models\Role $role
+    * @return bool
+    */
+    public function hasRole(Role $role)
+    {
+        foreach( $this->roles as $r ) {
+            ($r->id != $role->id) ?: return true;
+        }
+
+        return false
+    }
+
+    /**
+    * Returns if the user has a role by name.
+    * @param string $role
+    * @return bool
+    */
+    public function hasRoleByName($name)
+    {
+        foreach( $this->roles as $r ) {
+            ($name != $role->name) ?: return true;
+        }
+
+        return false
+    }
+
     /**
     * Returns the user avatar.
     */
