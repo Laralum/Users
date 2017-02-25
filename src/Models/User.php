bf7ae8a97dc35f7c69598a0188e87c08d317d024
Erik Campobadal <soc@erik.cat>
2017-02-25 14:25:30 +0100

    Gavatar

--- src/Models/User.php
+++ src/Models/User.php
@@ -69,12 +69,18 @@ class User extends ExtendUser
     /**
     * Returns the user avatar.
     */
-    public function avatar()
+    public function avatar($size = 100)
     {
+        /*
         if(File::exists(public_path('/avatars'.'/'.md5($this->email)))){
             return asset('/avatars'.'/'.md5($this->email));
         }
         return "https://tracker.moodle.org/secure/attachment/30912/f3.png";
+        */
+        // Get gavatar avatar
+        $email = md5(strtolower(trim($this->email)));
+        $default = urlencode("https://tracker.moodle.org/secure/attachment/30912/f3.png");
+        return "https://www.gravatar.com/avatar/$email?d=$default&s=$size";
     }
 
     /**
@@ -82,9 +88,14 @@ class User extends ExtendUser
     */
     public function hasAvatar()
     {
-        if(File::exists(public_path('/avatars/'.md5($this->email)))){
+        /*
+        if (File::exists(public_path('/avatars/'.md5($this->email)))){
             return true;
         }
         return false;
+        */
+
+        // There's always a gavatar
+        return true;
     }
 }
