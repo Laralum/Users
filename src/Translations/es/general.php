e4ed9e53644878a8774f2b6526457f2d9d92e639
24aitor <contact@aitorriba.com>
2017-04-15 03:05:09 +0200

    Added Translations

---
+++ src/Translations/es/general.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'users' => 'Usuarios',
+    'home' => 'Inicio',
+    'user_list' => 'Lista de usuarios',
+    'users_desc' => 'Los usuarios le permitirán administrar fácilmente todos los usuarios de su sitio web y asignarles roles.',
+    'create' => 'Crear',
+    'create_user' => 'Crear usuario',
+    'name' => 'Nombre',
+    'create_user_desc' => 'Crear un nuevo usuario en la base de datos',
+    'password' => 'Contraseña',
+    'password_confirmation' => 'Confirmación de contraseña',
+    'edit' => 'Editar',
+    'edit_desc' => 'Estás editando el usuario :id creado :time_ago',
+    'edit_user' => 'Editar usuario',
+    'delete' => 'Borrar',
+    'actions' => 'Comportamiento',
+    'email' => 'Email',
+    'roles' => 'Roles',
+    'cancel' => 'Cancelar',
+    'submit' => 'Enviar',
+    'edit_yourself_error' => '¡Usa el perfil para editarte!',
+    'delete_yourself_error' => '¡No puedes eliminarte a ti mismo!',
+    'user_roles' => 'Roles del usuario',
+    'user_deleted' => 'Usuario #:id eliminado!',
+    'user_created' => 'Usuario con correo electrónico :email ha sido creado!',
+    'roles_desc' => 'Edición de los roles #:id del usuario.  Actualmente :total roles disponibles.',
+    'available_roles' => 'Roles disponibles',
+    'user_roles_updated' => '¡Roles de usuario #:id actualizadas!',
+    'new_users' => 'Usuarios nuevos',
+    'last_users' => 'Nuevos usuarios en los últimos 7 días',
+];
---
+++ src/Translations/it/general.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'users' => 'Utenti',
+    'home' => 'Casa',
+    'user_list' => 'Lista utenti',
+    'users_desc' => 'Gli utenti vi permetterà di gestire facilmente tutti gli utenti del sito web e assegnare loro ruoli.',
+    'create' => 'Creare',
+    'create_user' => 'Creare un utente',
+    'name' => 'Nome',
+    'create_user_desc' => 'Creare un nuovo utente al database',
+    'password' => "Parola d'ordine",
+    'password_confirmation' => 'Conferma password',
+    'edit' => 'Modifica',
+    'edit_desc' => 'Si sta modificando :time_ago Var0 utente creato',
+    'edit_user' => 'Modifica utente',
+    'delete' => 'Elimina',
+    'actions' => 'Azioni',
+    'email' => 'E-mail',
+    'roles' => 'Ruoli',
+    'cancel' => 'Annulla',
+    'submit' => 'Sottoscrivi',
+    'edit_yourself_error' => 'Usa profilo da modificare da soli!',
+    'delete_yourself_error' => 'Non è possibile cancellare da soli!',
+    'user_roles' => 'Ruoli utente',
+    'user_deleted' => 'Var0 Utente cancellato!',
+    'user_created' => "L'utente con l'email :email è stato creato!",
+    'roles_desc' => 'Utente Editing Var0 ruoli.  Attualmente VAR1 ruoli disponibili.',
+    'available_roles' => 'Ruoli disponibili',
+    'user_roles_updated' => 'Ruoli Var0 utente aggiornata!',
+    'new_users' => 'Nuovi utenti',
+    'last_users' => 'I nuovi utenti negli ultimi 7 giorni',
+];
